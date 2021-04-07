bdf83e3f0e937cfab01068b5d65e5c9746c00038
Alkarp13 <alexey.karpukhin2011@yandex.ru>
2021-04-07 21:00:40 +0300

    test avatar component

--- www/src/components/PersonsRow.tsx
+++ www/src/components/PersonsRow.tsx
@@ -3,8 +3,6 @@ import { PersonInfo } from './PersonCard';
 import Panel from './Primitives/Panel/Panel';
 import Avatar from './Primitives/Avatar/Avatar';
 import ReactTooltip from 'react-tooltip';
-// const ReactHintFactory = require('react-hint');
-// const ReactHint = ReactHintFactory(React);
 
 interface PersonsQuery {
     id      : number;
@@ -33,39 +31,19 @@ export default function PersonsRow(props: Props) {
                 background: "rgba(0, 0, 0, 0.3)"
             }}>
             <ReactTooltip id='avatar_hint' getContent={(dataTip) => `${dataTip}`}/>
-            {['aaaa', 'bbbbb', 'ccccc', 'dddd', 'eeeeeeee', 'fffffff'].map((person: string, index: number) => {
+            {props.other_persons.map((person: PersonInfo, index: number) => {
                 return (
                     <Avatar
                         key={index}
                         dataFor='avatar_hint'
-                        dataTip={person + ", " + person}
-                        name={person}
+                        dataTip={person.username + ", " + person.first_name}
+                        name={person.username}
                         size={80}
                         marginRight={16}
-                        isSolid={false}
-                        onClick={(e: React.MouseEvent<HTMLElement>) => {console.log(person)}}
+                        isSolid={(props.persons_query[props.current_person - 1].username === person.username) ? true : false}
+                        onClick={(e: React.MouseEvent<HTMLElement>) => {props.showOtherPerson(e)}}
                     />
                 )})}
         </Panel>
     );
 }
-
-// {props.other_persons.map((person: PersonInfo, index: number) => {
-//     return <Panel key={index}
-//         style={{
-//             float: "left",
-//             justifyContent: "left",
-//             alignItems: "center",
-//             width: "80px"
-//     }}>
-//         <ReactHint autoPosition events />
-//         <Avatar
-//             name={person.username}
-//             size={80}
-//             data-rh={person.username + ", " + person.first_name}
-//             marginRight={16}
-//             isSolid={(props.persons_query[props.current_person - 1].username === person.username) ? true : false}
-//             onClick={(e: React.MouseEvent<HTMLElement>) => {props.showOtherPerson(e)}}
-//         />
-//     </Panel>
-// })}
